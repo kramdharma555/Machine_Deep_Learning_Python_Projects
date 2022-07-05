6048182d045157fdf5b05650eba8b6897c7cf59b
kramdharma555 <39027196+kramdharma555@users.noreply.github.com>
2022-07-05 14:05:58 +0530

    Update SpellingCorrection.py

--- SpellingCorrection.py
+++ SpellingCorrection.py
@@ -1,15 +1,19 @@
 #We have to import the TextBlob Module for this spelling correction
 from textblob import TextBlob
 #words user defined variable/obeject is used to hold the sentences
-words = ["Cmptr", "Scnce", "Lngueg"]
+Example_words = ["Cmptr", "Scnce", "Lngueg"]
 #Correctword holds the dummy arrays to store many values 
 CorrectWord = []
 #For loop is performed here to get the number of characters/words will be appended 
-for i in words:
+for i in Example_words:
     CorrectWord.append(TextBlob(i))
 #For testing the mistaken words
-print("Mistaken Word", words)
+print("Mistaken Word", Example_words)
 #To get the corrected words and one more loop is being used to show each corrected word
-print("Corrected Word is")
+print("Corrected Words are Listed ")
 for i in CorrectWord:
     print(i.correct()), end="")
+
+    
+    
+ 
